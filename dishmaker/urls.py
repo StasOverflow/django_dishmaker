ec6d28e5cf56bf8b670a627f82cf93e884d83fbc
Vladimir Striukov <vladimir.alexeevich.morrowind@gmail.com>
2019-02-27 01:00:31 +0300

    moving on with dish profile page

--- dishmaker/urls.py
+++ dishmaker/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, DishView
 
 app_name = 'dishmaker'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('<int:dish_id>/', DishView.as_view(), name='dish'),
 ]
--- dishmaker/views.py
+++ dishmaker/views.py
@@ -25,6 +25,34 @@ class IndexView(ListView, BaseKindaAbstractView):
     def get_context_data(self, **kwargs):
         context = BaseKindaAbstractView.get_context_data(self, **kwargs)
         context['title'] = self.title
+        recipes = DishRecipe.objects.prefetch_related('ingredients').all()
+        context['recipes'] = recipes
+        return context
+
+        # return render(request, template_name=self.template_name, context={'title': self.title})
+
+
+def dish_profile(request, dish_id):
+    # cnt = request.session.get('cnt', 0)
+    # cnt += 1
+    # request.session['cnt'] = cnt
+    print(dish_id)
+    # post = .objects.filter(id=post_id).first()
+    # print(post)
+    recipes = DishRecipe.objects.prefetch_related('ingredients').all()
+    # ingredients_in_recipes =
+    # ingredients =
+    return render(request, 'dishmaker/content/index.html', context={'recipes': recipes, 'dish': recipes[0]})
+
+
+class DishView(ListView, BaseKindaAbstractView):
+    template_name = "dishmaker/content/index.html"
+    title = "Recipes list"
+    model = DishRecipe
+
+    def get_context_data(self, **kwargs):
+        context = BaseKindaAbstractView.get_context_data(self, **kwargs)
+        context['title'] = self.title
         # prefetch_related is like select_related, but for many to many cases
         recipes = DishRecipe.objects.prefetch_related('ingredients').all()
         # ingredients_in_recipes =
