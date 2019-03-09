d6c951c22f51f8c12e16ec62416909583455160e
Vladimir Striukov <vladimir.alexeevich.morrowind@gmail.com>
2019-03-09 08:30:53 +0300

    remade permissions for chef

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,18 +1,23 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import gettext_lazy as _
+from dishmaker.models import Dish, Ingredient, Order
+from dishmaker.models import NotedItem
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
 
 
 class DishmakerUserCreationForm(UserCreationForm):
 
-    CHOICES = [('Chef', 'Chef'),
-               ('Customer', 'Customer')]
+    CHOICES = [('Chef', _('Chef')),
+               ('Customer', _('Customer'))]
 
     sign_up_as = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["sign_up_as"].label = "Sign up as"
+        self.fields["sign_up_as"].label = _("Sign up as")
 
     class Meta:
         model = User
@@ -21,7 +26,24 @@ class DishmakerUserCreationForm(UserCreationForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         if self.cleaned_data["sign_up_as"] == "Chef":
+            user.save()
             user.is_staff = True
+            content_type = ContentType.objects.get_for_model(Dish)
+            dish_permissions = Permission.objects.filter(content_type=content_type)
+            content_type = ContentType.objects.get_for_model(Ingredient)
+            ingredients_permissions = Permission.objects.filter(content_type=content_type)
+            content_type = ContentType.objects.get_for_model(Order)
+            order_permissions = Permission.objects.filter(content_type=content_type)
+            content_type = ContentType.objects.get_for_model(Order)
+            noted_permissions = Permission.objects.filter(content_type=content_type)
+            for perm in dish_permissions:
+                user.user_permissions.add(perm)
+            for perm in ingredients_permissions:
+                user.user_permissions.add(perm)
+            for perm in order_permissions:
+                user.user_permissions.add(perm)
+            for perm in noted_permissions:
+                user.user_permissions.add(perm)
         if commit:
             user.save()
         return user
