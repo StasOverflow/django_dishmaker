2ec4322176fa7ba52ebc8e94353c11b1f28d8351
Vladimir Striukov <vladimir.alexeevich.morrowind@gmail.com>
2019-03-09 07:26:33 +0300

    added dishmaker template translations

--- dish_composer/settings.py
+++ dish_composer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -106,7 +107,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGES = [
+    ('en', _('English')),
+    ('ru', _('Russian')),
+]
+
+LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'), )
+
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
