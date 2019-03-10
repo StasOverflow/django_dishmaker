c5c6f95423ce59f2ba309ddc7cc09645d63ee098
Vladimir Striukov <vladimir.alexeevich.morrowind@gmail.com>
2019-03-10 05:22:45 +0300

    fixed author displaying

--- dishmaker/serializers.py
+++ dishmaker/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from dishmaker.models import Order, Dish, Ingredient, IngredientQuantityInDishProxy
 from notes.models import NotedItem
 from rest_framework.serializers import RelatedField
+from django.contrib.auth.models import User
 
 
 class IngredientsSerializer(serializers.ModelSerializer):
@@ -20,15 +21,22 @@ class IngredientQuantitySerializer(serializers.HyperlinkedModelSerializer):
         fields = ('name', 'ingredient_quantity')
 
 
+class AuthorRelatedField(RelatedField):
+
+    def to_representation(self, value):
+        return value.username
+
+
 class DishSerializer(serializers.ModelSerializer):
+
     ingredients = IngredientQuantitySerializer(source='dishrecipe', many=True)
+    author = AuthorRelatedField(queryset=User.objects.all())
 
     class Meta:
         model = Dish
         fields = ('name', 'description', 'ingredients', 'created_on', 'author')
 
     def create(self, validated_data):
-        print(validated_data )
         ings_data = validated_data.pop('dishrecipe')
         dish = Dish.objects.create(**validated_data)
         for ing in ings_data:
@@ -42,9 +50,27 @@ class DishSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
+
+    ingredients = IngredientQuantitySerializer(source='order', many=True)
+    dish_id = serializers.PrimaryKeyRelatedField(source='dish_id.name', queryset=Dish.objects.all())
+
+    author = AuthorRelatedField(queryset=User.objects.all())
+
     class Meta:
         model = Order
-        fields = ('id', 'description', 'ingredients', 'created_on', 'author', 'is_active')
+        fields = ('id', 'dish_id', 'description', 'ingredients', 'created_on', 'author', 'is_active')
+
+    def create(self, validated_data):
+        ings_data = validated_data.pop('dishrecipe')
+        order = Order.objects.create(**validated_data)
+        for ing in ings_data:
+            name = ing.get('ingredient_id').get('name')
+            quantity = ing.get('ingredient_quantity')
+            ingredient_database_instance = Ingredient.objects.filter(name=name).first()
+            '''order here is a related name'''
+            order.order.create(ingredient_id=ingredient_database_instance,
+                               ingredient_quantity=quantity)
+        return order
 
 
 class NotedItemSerializer(serializers.ModelSerializer):
